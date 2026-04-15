7cfb3c2ab4084c19ff8eb7ee205ef435e1f13f86
nazeerou <naziru2007@gmail.com>
2026-04-15 10:47:20 +0300

    fixed

--- resources/views/estimations/invoice-pdf.blade.php
+++ resources/views/estimations/invoice-pdf.blade.php
@@ -146,7 +146,7 @@ pointer-events: none;
         }
         footer {
             position: fixed; 
-            bottom: -105px; 
+            bottom: -106px; 
             left: 0px; 
             right: 0px;
             height: 115px; 
