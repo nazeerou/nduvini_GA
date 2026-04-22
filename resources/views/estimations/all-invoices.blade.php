e742fe020cc73195a2279fd59c5d9f305326ad20
nazeerou <naziru2007@gmail.com>
2026-04-22 11:03:02 +0300

    fix

--- resources/views/estimations/all-invoices.blade.php
+++ resources/views/estimations/all-invoices.blade.php
@@ -149,8 +149,6 @@
                       </div>
                      </div> <hr/>
                      <input type="hidden" name="id" id="id" class="form-control">
-
-                     <!-- @if (Auth::user()->role_id === 1 || Auth::user()->role_id === 2) -->
                         <div class="row">
                         <div class="col-md-4">
                             <div class="form-group">
@@ -174,7 +172,6 @@
                             </div>
                         </div>
                     </div>
-                    <!-- @endif -->
                     <div class="row">
                         <div class="col-md-4">
                             <div class="form-group">
