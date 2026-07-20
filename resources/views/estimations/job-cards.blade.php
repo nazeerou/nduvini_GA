c8ffa7e2146c855165694c2ac521e8cbc4e9e74e
nazeerou <naziru2007@gmail.com>
2026-07-20 09:48:09 +0300

    fixed

--- resources/views/estimations/job-cards.blade.php
+++ resources/views/estimations/job-cards.blade.php
@@ -65,9 +65,11 @@
                                      <a href="#">{{ $product->job_card_no }}</a>
                                 </td>
                                 <td width="150px">
-                             {{ 
-                                  $product->delivery_date ?? $product->created_date
-                              }}
+                               {{ 
+                      !empty($product->delivery_date)
+                          ? \Carbon\Carbon::parse($product->delivery_date)->format('Y M, d')
+                          : \Carbon\Carbon::parse($product->created_date)->format('Y M, d')
+                  }}
                                 </td>
                                 <td> 
                               @if ($product->invoice_number == NULL)
