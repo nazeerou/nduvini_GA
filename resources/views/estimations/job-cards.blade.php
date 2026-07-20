f1d6aa95995adf26e217ef9f38777187fbc079c6
nazeerou <naziru2007@gmail.com>
2026-07-20 09:42:14 +0300

    fixed

--- resources/views/estimations/job-cards.blade.php
+++ resources/views/estimations/job-cards.blade.php
@@ -65,10 +65,10 @@
                                      <a href="#">{{ $product->job_card_no }}</a>
                                 </td>
                                 <td width="150px">
-                               {{ 
+                              {{ 
                                     !empty($product->delivery_date)
-                                        ? \Carbon\Carbon::parse($product->delivery_date)->format('Y M, d')
-                                        : \Carbon\Carbon::parse($product->created_date)->format('Y M, d')
+                                        ? \Carbon\Carbon::createFromFormat('d/m/Y', $product->delivery_date)->format('Y M, d')
+                                        : \Carbon\Carbon::createFromFormat('d/m/Y', $product->created_date)->format('Y M, d')
                                 }}
                                 </td>
                                 <td> 
